d65778d7038d5e204e2893eaa5b70e24398da81a
Elijah wines <elijahwines84@yahoo.com>
2020-08-13 17:27:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   // your code here
-
+  let submit = document.getElementById('create-task-form').children[2];
+  let newTask = document.getElementById('new-task-description').value;
+  
 });
