9f4c2c4e7c0e67a427bab235808bd0089a86bb83
Elijah wines <elijahwines84@yahoo.com>
2020-08-13 17:24:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) => {
 
   // your code here
-  let taskList = document.getElementById("tasks");
-  let submit = document.querySelector('form#create-task-form').children[2];
-
-  submit.addEventListener("click", function(e) {
-    let newTask = document.getElementById("new-task-description");
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(newTask.value));
-    taskList.appendChild(li);
-    e.preventDefault()
-  });
 
 });
